dc9eee5a4557196253db54be103029ba7dd31049
zx <1013230955@qq.com>
2019-07-26 20:38:01 +0800

    添加项目文件。

---
+++ WindowsFormsApp9/Form1.Designer.cs
@@ -0,0 +1,79 @@
+﻿namespace WindowsFormsApp9
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.button10 = new System.Windows.Forms.Button();
+            this.fontDialog1 = new System.Windows.Forms.FontDialog();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button10
+            // 
+            this.button10.Location = new System.Drawing.Point(166, 43);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(75, 23);
+            this.button10.TabIndex = 11;
+            this.button10.Text = "加载代码";
+            this.button10.UseVisualStyleBackColor = true;
+            this.button10.Click += new System.EventHandler(this.button10_Click_1);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(166, 103);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 12;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(655, 463);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.button10);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.FontDialog fontDialog1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ WindowsFormsApp9/Form1.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+namespace WindowsFormsApp9
+{
+    public partial class Form1 : Form
+    {
+        Thread drawThread = null;
+        static Thread thread1, thread2;
+        static string fileName;
+        static string curFileName;
+
+        static System.Drawing.Bitmap curBitmap;
+        static Thread thread3;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+
+
+        public void button10_Click_1(object sender, EventArgs e)
+        {
+            try
+            {
+                closeThread();
+                drawThread = new Thread(new ParameterizedThreadStart(thread));
+                drawThread.SetApartmentState(ApartmentState.STA);
+                drawThread.Start(); this.SetStyle(ControlStyles.UserPaint, true);
+                this.SetStyle(ControlStyles.OptimizedDoubleBuffer, true);
+                this.SetStyle(ControlStyles.AllPaintingInWmPaint, true);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+        //关闭子线程
+        private void closeThread()
+        {
+            if (drawThread != null)
+            {
+                if (drawThread.IsAlive)
+                {
+                    drawThread.Abort();
+                }
+            }
+        }
+
+
+       
+       
+
+
+
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            closeThread();           
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveFile = new SaveFileDialog();
+            saveFile.Title = "保存为";
+            saveFile.OverwritePrompt = true;          
+            saveFile.ShowHelp = true;
+            if (saveFile.ShowDialog() == DialogResult.OK)
+            {
+                string curFileName = saveFile.FileName;
+                byte[] buff = new byte[1];
+                FileStream fileStream = new FileStream(curFileName, FileMode.Create);
+                BinaryWriter binaryWriter = new BinaryWriter(fileStream);
+                binaryWriter.Write(buff, 0, buff.Length);
+                binaryWriter.Close();
+                fileStream.Close();
+            }
+        }
+
+        //加载代码
+        static void thread(Object dataobj)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.Filter = "|*.nc;*.MP4; *.png; *.gif; *.jpeg; *.bmp";
+            dialog.ShowDialog();
+            try
+            {
+                fileName = dialog.FileName.ToString();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }                 
+    }
+}
